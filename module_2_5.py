81d2f96576a0f1f43910ff33ab450b2d83ad1638
Nikon-33 <89101717800@mail.ru>
2024-07-03 15:41:33 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,12 @@
+def get_matrix(n, m, value):
+    matrix = [[value for i in range(m)] for x in range(n)]
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+
